87b9e2b8d0a37ce94cd68a89111ea55d301a479f
Blueevee <evelyn.ferreira@solutis.com.br>
2023-09-23 09:27:57 -0300

    fix: change received genbank accession id

--- plasticome/controllers/fungi_controller.py
+++ plasticome/controllers/fungi_controller.py
@@ -2,8 +2,7 @@ from plasticome.services.genbank_service import search_fungi_id_by_name
 
 
 def search_fungi_by_name(fungi_name: str):
-    response = search_fungi_id_by_name(fungi_name)
-
-    if int(response['Count']) == 0:
-        return {'message': 'Genome not found'}, 404
-    return {'fungi_id': response['IdList'][0]}, 200
+    genbank_accession, refseq_accession = search_fungi_id_by_name(fungi_name)
+    if genbank_accession or refseq_accession:
+        return {'genbank_assembly_accession': genbank_accession, 'refseq_assembly_accession': refseq_accession}, 200
+    return {'message': 'Genome not found'}, 404
