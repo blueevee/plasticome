ef130f2f4b683f291a409218fe3f8dbd21f2fa32
Blueevee <evelyn.ferreira@solutis.com.br>
2023-10-20 11:11:40 -0300

    feat: add plasticome metadata querys

---
+++ plasticome/services/auth_user_service.py
@@ -0,0 +1,21 @@
+
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv(override=True)
+
+def authenticate_user(username: str, secret: str):
+
+    auth_data = {
+    "username": username,
+    "secret": secret
+    }
+
+    response = requests.post(f"{os.getenv('PLASTICOME_METADATA_URL')}/auth", json=auth_data)
+
+    if response.status_code == 200:
+        data = response.json()
+        token = data.get("access_token")
+        return token, False
+    return False, True
---
+++ plasticome/services/plasticome_metadata_service.py
@@ -0,0 +1,50 @@
+
+import os
+from dotenv import load_dotenv
+import requests
+from plasticome.services.auth_user_service import authenticate_user
+
+load_dotenv(override=True)
+
+def get_all_enzymes(username: str, secret: str):
+
+    token, error = authenticate_user(username, secret)
+    if error:
+        return False, True
+
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(f'{os.getenv("PLASTICOME_METADATA_URL")}/enzyme_find', headers=headers)
+
+    data = response.json()
+    if response.status_code == 200:
+        return data, False
+    return False, data
+
+def get_all_plastics_with_enzymes(username: str, secret: str):
+
+    token, error = authenticate_user(username, secret)
+    if error:
+        return False, True
+
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(f'{os.getenv("PLASTICOME_METADATA_URL")}/plastic_enzyme_find', headers=headers)
+
+    data = response.json()
+    if response.status_code == 200:
+        return data, False
+    return False, data
+
+def get_all_plastic_types_by_enzyme(username: str, secret: str, enzyme_id: int):
+
+    token, error = authenticate_user(username, secret)
+    if error:
+        return False, True
+
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(f'{os.getenv("PLASTICOME_METADATA_URL")}/plastic_enzyme_find/{enzyme_id}', headers=headers)
+
+    data = response.json()
+    if response.status_code == 200:
+        plastic_values = [item["plastic"] for item in data]
+        return plastic_values, False
+    return False, data
