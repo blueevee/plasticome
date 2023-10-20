db5c10dc99b38b67886592cdcd07a1b18dec9e1c
Blueevee <evelyn.ferreira@solutis.com.br>
2023-10-20 11:11:57 -0300

    feat: add email validation

--- plasticome/services/Helpers.py
+++ plasticome/services/Helpers.py
@@ -2,6 +2,15 @@ import re
 
 
 def validate_email(email: str):
+    """
+    The function `validate_email` checks if a given email address is valid
+    according to a regular expression pattern.
+
+    :param email: The parameter `email` is a string that represents an email
+    address
+    :return: The function `validate_email` returns a boolean value. It returns
+    `True` if the email is valid and `False` if the email is not valid.
+    """
     email = email.strip()
     if re.match(r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", email) == None:
         return False
